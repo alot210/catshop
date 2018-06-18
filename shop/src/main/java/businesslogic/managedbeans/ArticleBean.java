9bc6eb8ac81c01071156d3ce25d247035785448a
JanoschMS <meetja@gmx.de>
2018-06-18 15:55:22 +0200

    articlebean

--- shop/src/main/java/businesslogic/managedbeans/ArticleBean.java
+++ shop/src/main/java/businesslogic/managedbeans/ArticleBean.java
@@ -1,4 +1,44 @@
 package businesslogic.managedbeans;
+import businesslogic.manager.ArticleManager;
+import transferModels.Article;
 
-public class ArticleBean {
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+@ManagedBean
+public class ArticleBean implements Serializable{
+
+    private Article article;
+    private ArticleManager articleManager;
+
+    void init(){
+
+        articleManager = new ArticleManager();
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public ArticleManager getArticleManager() {
+        return articleManager;
+    }
+
+    public void setArticleManager(ArticleManager articleManager) {
+        this.articleManager = articleManager;
+    }
+
+    public String getArticles(){
+
+        articleManager.getArticles();
+        return "products.xhtml";
+    }
+
+
+
+
 }
