595e4a4154e5a37665fbe04db2b90c1509054d21
alina <alinaotten@web.de>
2018-06-18 14:29:39 +0200

    add web folder and implement config logic

--- shop/src/main/java/transferModels/Config.java
+++ shop/src/main/java/transferModels/Config.java
@@ -1,7 +1,20 @@
 package transferModels;
 
+import java.sql.Connection;
+
 import java.io.Serializable;
 
-//TODO
 public class Config implements Serializable {
+
+
+    static Connection con;
+
+    public static Connection  setSQLConnection(Connection _con){
+        con = _con;
+        return con;
+    }
+
+    public static Connection getSQLConnection(){
+        return con;
+    }
 }
