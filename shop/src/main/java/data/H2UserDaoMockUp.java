a9e8eb918dcef691f876ad72103b08a75b38ead5
Daphne <daphne.waporidis@innoq.com>
2018-06-22 13:33:01 +0200

    add db mock up class

--- shop/src/main/java/data/H2UserDaoMockUp.java
+++ shop/src/main/java/data/H2UserDaoMockUp.java
@@ -1,4 +1,25 @@
 package data;
 
-public class H2UserDaoMockUp {
+import transferModels.Article;
+import transferModels.User;
+
+import java.sql.SQLException;
+
+public class H2UserDaoMockUp implements UserDao {
+
+    @Override
+    public void addUser(User article) throws SQLException {
+    }
+
+    @Override
+    public User getUser(int id) throws SQLException {
+        String username ="wiebke";
+        User user = new User(username, "1234", username.hashCode());
+        return user;
+    }
+
+    @Override
+    public void deleteUser(User article) {
+
+    }
 }
