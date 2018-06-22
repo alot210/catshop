84aa85c3bf028e3868eaa0ba59da51eb7e400da3
JanoschMS <meetja@gmx.de>
2018-06-22 14:38:50 +0200

    login speicherung und weiterleitung

--- shop/src/main/java/businesslogic/manager/UserManager.java
+++ shop/src/main/java/businesslogic/manager/UserManager.java
@@ -16,12 +16,12 @@ public class UserManager {
     public void createUser(String userName, String password, int id){
 
         User u = new User(userName,password,id);
-        H2UserDao UserDao = H2FactoryDao.getDaoUser();
+        /*H2UserDao UserDao = H2FactoryDao.getDaoUser();
         try {
-            UserDao.addUser(u);
+            //UserDao.addUser(u);
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
 
